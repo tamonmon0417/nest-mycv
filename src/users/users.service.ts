a113a3e25f66979ba03b1a2d4244035d0098e2c2
tamonmon0417 <tamonmon0417@gmail.com>
2022-03-27 15:20:31 +0900

    add methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,10 +4,45 @@ import { Repository } from 'typeorm';
 import { User } from './users.entity';
 @Injectable()
 export class UsersService {
+  // business logic here
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
   create(email: string, password: string) {
+    // create user entity instance
+    // creating user instance enables hooks of entity.
     const user = this.repo.create({ email, password });
+
+    // hooks executed by save()
     return this.repo.save(user);
   }
+
+  findOne(id: number) {
+    return this.repo.findOne(id);
+  }
+
+  find(email: string) {
+    return this.repo.find({ email });
+  }
+
+  // specific args are not good design
+  // attrs: attributes
+  // const usersService = new UsersService({} as any);
+  // usersService.update(1, { email: 'a' });
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('user not found');
+    }
+
+    // override properties already there
+    Object.assign(user, attrs);
+
+    // hooks executed
+    return this.repo.save(user);
+  }
+
+  remove() {}
 }
+
+const usersService = new UsersService({} as any);
+usersService.update(1, { email: 'a' });
